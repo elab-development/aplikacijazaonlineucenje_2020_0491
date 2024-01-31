55dbbfc24b07ebf208fa2ab40cd1c63a237e2a13
Zeljk01 <zivago59@gmail.com>
2024-01-31 22:58:49 +0100

    controllers and routes for autorization

--- event-planner/routes/api.php
+++ event-planner/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AuthorizationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [AuthorizationController::class, 'register']);
+Route::post('/login', [AuthorizationController::class, 'login']);
+
+Route::post('/register', [AuthorizationController::class, 'register']);
+Route::post('/login', [AuthorizationController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
