20d22f7f894e0509f4691dc84b835c72b0204936
TeodoraObucina <teodoraobucina22@gmail.com>
2024-02-03 10:56:22 +0100

    searchform update2

--- open-library/src/components/SearchForm/SearchForm.jsx
+++ open-library/src/components/SearchForm/SearchForm.jsx
@@ -14,12 +14,12 @@ const SearchForm = () => {
   useEffect(() => searchText.current.focus(), []);
   const handleSubmit = (e) => {
     e.preventDefault();
-    let tempSearchTerm = searchText.current.valueOf.trim();
-    if (tempSearchTerm.replace(/[^\w\s]/gi, "").length === 0) {
+    let tempSearchTerm = searchText.current.value.trim();
+    if (tempSearchTerm.replace(/[^\w\s]/gi, "").length == 0) {
       setSearchTerm("lord of the rings");
       setResultTitle("Please Enter Something ...");
     } else {
-      setSearchTerm(searchText.current.valueOf);
+      setSearchTerm(searchText.current.value);
     }
 
     navigate("/book");
